608ee4703533650a3b37e4f19fde376fb180d2d4
manukanna <84427478+manukanna@users.noreply.github.com>
2023-05-10 06:41:34 +0530

    Delete src directory

--- src/fetchProducts/fetchProducts.js
+++
@@ -1,8 +0,0 @@
-import { createAsyncThunk } from "@reduxjs/toolkit";
-import axios from 'axios'
-
-
-export const storePoducts = createAsyncThunk('products/getStoreProducts', () => {
-    return axios.get("https://fakestoreapi.com/products")
-        .then(res => res.data)
-})
